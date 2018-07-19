7da1224e7450288d5386f0b71007ec120c1c5482
Peter Kalambet <peter@technogeek.io>
2018-07-19 11:52:13 +0200

    Add sleep to airdrop script

--- tools/airdrop.js
+++ tools/airdrop.js
@@ -1,6 +1,5 @@
 var SKYFToken = artifacts.require("./SKYFToken.sol");
 var investors = require('./investors.0.js');
-var util = require("util");
 
 module.exports = async function(callback) {
 
@@ -8,13 +7,18 @@ module.exports = async function(callback) {
         console.log(e);
     };
 
-    var token = await SKYFToken.deployed()
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    var token = await SKYFToken.at("0xbe4c3eBf48154bd7385D332F846baa421c499027");
 
     var addresses = Object.getOwnPropertyNames(investors.investor);
     for (let val of addresses ) {
         await token.addAirdrop(val, web3.toWei(investors.investor[val], "ether")).then(function() {
             console.log("address (" + val + ") -> rate (" + investors.investor[val] + ")");
         }).catch(fail);
+        await sleep(5000);
     }
     
     callback();
