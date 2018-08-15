a0d23dd9c905f6c0aae05d6a5d9e689763e73389
valentin.rul <v.rul@qiwi.com>
2018-08-15 17:44:09 +0300

    Added bounty.js for sending bounty tokens. Fixed addresses in crowdsale.js

--- tools/bounty.js
+++ tools/bounty.js
@@ -11,13 +11,12 @@ module.exports = async function(callback) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    var crowdsaleAddress = "0x79cee3a98afc7f46436e71b452751b8b936b7129";
-
+    
     var token = await SKYFToken.at("0x5dd0815a4cf119ad91ba045bbbf879f3f7de3c68");
 
     var addresses = Object.getOwnPropertyNames(investors.investor);
     for (let val of addresses ) {
-        await token.transferFrom(crowdsaleAddress, val, web3.toWei(investors.investor[val], "ether")).then(function() {
+        await token.transfer(val, web3.toWei(investors.investor[val], "ether")).then(function() {
             console.log("address (" + val + ") -> rate (" + investors.investor[val] + ")");
         }).catch(fail);
         await sleep(5000);
